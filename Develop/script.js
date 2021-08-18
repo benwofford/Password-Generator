7b0623c1b32525c8b54cf5bb68f944d6d22399f3
BenWofford <woffordbt@gmail.com>
2021-08-18 19:47:44 -0400

    built out vars

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowerChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+var upperChar = ['K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'];
+var numberChar = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +14,12 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+function generatePassword() {
+  var question = prompt("Would you like to include lower case characters?");
+  if (
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
